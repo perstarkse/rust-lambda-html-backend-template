cb016f68c56e6961822252abe706375adc5c38f6
Per Stark <perstark.se@gmail.com>
2023-11-11 20:22:18 +0100

    init backend

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "rust backend development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [ pkgs.bashInteractive pkgs.cargo-lambda pkgs.rustc pkgs.rustup ];
+      };
+    });
+}
